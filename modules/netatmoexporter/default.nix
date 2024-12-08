3d95dd987964037bab799491df45147a69c28e46
J.P.Klippel <git@kl1pp3l.de>
2024-12-08 19:58:10 +0100

    fix module

--- modules/netatmoexporter/default.nix
+++ modules/netatmoexporter/default.nix
@@ -64,7 +64,7 @@ in
 
       package = mkOption {
         type = types.package;
-        default = pkgs.netatmo-exporter;
+        default = pkgs.netatmoexporter;
         defaultText = "pkgs.netatmoexporter";
         description = "Package to use with systemd.";
       };
